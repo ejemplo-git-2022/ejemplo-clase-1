b1ba32644b851a70ebd3492cf9eb00ab2b36c27e
claudiozam <claudio.zanoszczyk@honou.com.ar>
2022-11-10 11:22:42 -0300

    ejemplo de cache

--- src/main/java/edu/curso/spring/primerospasos/util/EmailManagerImpl.java
+++ src/main/java/edu/curso/spring/primerospasos/util/EmailManagerImpl.java
@@ -2,6 +2,9 @@ package edu.curso.spring.primerospasos.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -12,21 +15,21 @@ public class EmailManagerImpl implements EmailManager {
 
 	private static Logger log = LoggerFactory.getLogger(EmailManagerImpl.class);
 
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Override
 	@Async
 	public void enviarEmail(String destino, String mensaje) {
 		log.info("Inicio del ennvio de email...");
-		for (int i = 0; i < 10; i++) {
-			log.info("Esperando... " + i);
-
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
 		log.info("Destino: " + destino);
 		log.info("Nensaje: " + mensaje);
+		
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(destino);
+		mailMessage.setSubject("Alta de nuevo producto");
+		mailMessage.setText(mensaje);
+		javaMailSender.send(mailMessage);
 		log.info("Fin del ennvio de email...");		
 	}
 }
